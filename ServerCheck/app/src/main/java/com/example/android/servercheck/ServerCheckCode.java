9abaedc96d7f8bed4c2e11e139c52f010c73b66a
1ntrepida <alexa.rockwell@stonybrook.edu>
2016-07-16 20:30:10 -0400

    more fixes

--- ServerCheck/app/src/main/java/com/example/android/servercheck/ServerCheckCode.java
+++ ServerCheck/app/src/main/java/com/example/android/servercheck/ServerCheckCode.java
@@ -1,33 +1,47 @@
 package com.example.android.servercheck;
 
-import java.io.File;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by Alexa on 7/16/2016.
  */
 public class ServerCheckCode {
 
-    public ServerCheckCode(){}
+    //region to rate
+    ArrayList<String> unParsed;
+    HashMap<String, String> data;
 
-    public boolean loadInfo()
-    {
+    public ServerCheckCode() {
+        data = new HashMap<String, String>();
+    }
+
+    public void loadInfo() {
 
     }
 
-    public void getInfo()
-    {
+    public void getInfo() {
+        Document doc = null;
         File input = new File("/tmp/input.html");
-        Document doc = Jsoup.parse(input, "UTF-8", "http://example.com/");
+        try {
+            doc = Jsoup.parse(input, "UTF-8", "http://www.mmoserverstatus.com/pokemon_go");
+        } catch (IOException e) {
+            System.out.println("Exception thrown :" + e);
+        }
 
-        Element content = doc.getElementById("content");
-        Elements links = content.getElementsByTag("a");
-        for (Element link : links) {
-            String linkHref = link.attr("href");
-            String linkText = link.text();
+        if (doc != null) {
+            Element content = doc.addClass("counter");
+            Elements links = content.getElementsByClass("white");
+            for (Element link : links) {
+                unParsed.add(link.text());
+            }
         }
     }
 
